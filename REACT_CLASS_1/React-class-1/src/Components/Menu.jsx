5c1a35c9497a98ad59a0b4f2f3adc378b3f85732
PulkitPradhan <pulkitpradhan440@gmail.com>
2026-04-07 11:59:25 +0530

    Update Menu.jsx

--- REACT_CLASS_1/React-class-1/src/Components/Menu.jsx
+++ REACT_CLASS_1/React-class-1/src/Components/Menu.jsx
@@ -1,19 +1,17 @@
-function Menu() {
-  return (
-    <nav className="menu">
-      <div className="logo-container">
-        <span className="logo-text">MEDICHAIN AI</span>
-      </div>
-      <ul className="nav-links">
-        <li><a href="#center">Home</a></li>
-        <li><a href="#next-steps">Steps</a></li>
-        <li><a href="https://github.com/vitejs/vite" target="_blank" rel="noopener noreferrer">Docs</a></li>
-      </ul>
-      <div className="menu-actions">
-        <button className="login-btn">Log In</button>
-      </div>
-    </nav>
-  );
-}
+class Menu extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Menu</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>sports</li>
+                    <li>Politics</li>
 
-export default Menu;
+                </ul>
+            </div>
+        )
+
+    }
+}
+export default Menu
